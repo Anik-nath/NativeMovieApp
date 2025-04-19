128547d6b0ef9deb721187a93be82ffdcba86c07
Anik-nath <anikdevnath03112000@gmail.com>
2025-04-19 21:39:10 +0600

    initialize working branch

--- app/index.tsx
+++ app/index.tsx
@@ -4,6 +4,7 @@ export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-4xl text-dark-200">Welcome Native</Text>
+      <Text className="text-4xl text-dark-200">Anik Nath</Text>
     </View>
   );
 }
